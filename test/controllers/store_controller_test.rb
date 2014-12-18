ae5ae9a0b51127d181d08ee5a61f56452b7c6b28
Justin Hiltz <justin4770@gmail.com>
2014-12-17 21:04:10 -0500

    Add a dash of AJAX

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
+    assert_select '.store .entry > img', 3
+    assert_select '.entry input[type=submit]', 3
     assert_response :success
     assert_select '#columns #side a', minimum: 4
     assert_select '#main .entry', 3
